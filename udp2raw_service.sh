f5171ef19fccf492f018954c856006e82c1fef8f
Kris·Allen <i@atdawn.cc>
2022-10-24 22:08:44 +0800

    1.0.0

---
+++ udp2raw_service.sh
@@ -0,0 +1,22 @@
+#!/bin/sh /etc/rc.common
+# Copyright (C) 2022 Kris Allen i@atdawn.cc
+
+START=89
+STOP=11
+
+USE_PROCD=1
+
+if [ ! $UDP2RAW_HOME ];then
+    UDP2RAW_HOME="/etc/udp2raw"
+fi
+
+start_service() {
+    procd_open_instance
+    procd_set_param command $UDP2RAW_HOME/udp2raw --conf-file $UDP2RAW_HOME/config.conf
+    procd_set_param user root
+    procd_set_param stdout 1
+    procd_set_param stderr 1
+    procd_set_param pidfile $UDP2RAW_HOME/pid
+    procd_set_param respawn
+    procd_close_instance
+}
